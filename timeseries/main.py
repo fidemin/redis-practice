397c21c2cdcf3e7b7194fc8d5d3967974a8de654
Yunhong Min <yhmin84@gmail.com>
2019-06-23 21:13:25 +0900

    timeseries with zset 추가

--- timeseries/main.py
+++ timeseries/main.py
@@ -1,11 +1,21 @@
 
-from timeseries import TimeSeries, TimeSeriesWithHash
+from timeseries import TimeSeries, TimeSeriesWithHash, TimeSeriesWithZset
 from redis import Redis
 
-if __name__ == '__main__':
-    client = Redis(port=9005, db=0) 
-    client.flushdb()
-    #ts = TimeSeries(client, 'purchases:item1')
+def timeseries(client):
+    ts = TimeSeries(client, 'purchases:item1')
+
+    ts.insert(0)
+    ts.insert(1)
+    ts.insert(1)
+    ts.insert(3)
+    ts.insert(61)
+
+    results = ts.fetch('1day', 0, 4)
+    print(results)
+
+
+def timeseries_with_hash(client):
     ts = TimeSeriesWithHash(client, 'purchases:item1')
 
     ts.insert(0)
@@ -16,3 +26,31 @@ if __name__ == '__main__':
 
     results = ts.fetch('1day', 0, 4)
     print(results)
+
+
+def display_results(granularity_name, results):
+    print('Results from ' + granularity_name + ':')
+    print('Timestamp\t| Value')
+    print('-------- | ------')
+    for result in results:
+        print(str(result['timestamp']) + '\t| ' + str(result['value']))
+
+
+def timeseries_with_zset(client):
+    ts = TimeSeriesWithZset(client, 'concurrentplays')
+    begin_timestamp = 0
+    ts.insert(begin_timestamp, 'user:max')
+    ts.insert(begin_timestamp, 'user:max')
+    ts.insert(begin_timestamp+1, 'user:hugo')
+    ts.insert(begin_timestamp+1, 'user:renata')
+    ts.insert(begin_timestamp+3, 'user:hugo')
+    ts.insert(begin_timestamp+61, 'user:kc')
+
+    results = ts.fetch('1sec', begin_timestamp, begin_timestamp+4)
+    display_results('1sec', results)
+
+
+if __name__ == '__main__':
+    client = Redis(port=9005, db=0)
+    client.flushdb()
+    timeseries_with_zset(client)
--- timeseries/timeseries.py
+++ timeseries/timeseries.py
@@ -151,3 +151,75 @@ class TimeSeriesWithHash(object):
 
         return results
 
+
+class TimeSeriesWithZset(object):
+    def __init__(self, client, namespace):
+        self._namespace = namespace
+        self._client = client
+        self._units = {
+            'second': 1,
+            'minute': 60,
+            'hour': 60 * 60,
+            'day': 24 * 60 * 60
+        }
+        self._granularities = {
+            '1sec': {
+                'name': '1sec',
+                'ttl': self._units['hour'] * 2,
+                'duration': self._units['second'],
+                'hash_size': self._units['minute'] * 2
+            },
+            '1min': {
+                'name': '1min',
+                'ttl': self._units['day'] * 7,
+                'duration': self._units['minute'],
+                'hash_size': self._units['hour'] * 2
+            },
+            '1hour': {
+                'name': '1hour',
+                'ttl': self._units['day'] * 60,
+                'duration': self._units['hour'],
+                'hash_size': self._units['day'] * 5
+            },
+            '1day': {
+                'name': '1day',
+                'ttl': None,
+                'duration': self._units['day'],
+                'hash_size': self._units['day'] * 30
+            },
+        }
+
+    def insert(self, timestamp_in_seconds, thing):
+        for name, granularity in self._granularities.items():
+            key = self._key_name(granularity, timestamp_in_seconds)
+            timestamp_score = self._rounded_timestamp(timestamp_in_seconds, granularity['duration'])
+            member = str(timestamp_score) + ':' + thing
+            self._client.zadd(key, {member: timestamp_score})
+
+            if (granularity['ttl'] is not None):
+                self._client.expire(key, granularity['ttl'])
+
+    def fetch(self, granularity_name, begin_timestamp, end_timestamp):
+        granularity = self._granularities[granularity_name]
+        duration = granularity['duration']
+        begin = self._rounded_timestamp(begin_timestamp, duration)
+        end = self._rounded_timestamp(end_timestamp, duration)
+        keys = []
+
+        timestamp = begin
+
+        results = []
+        while timestamp <= end:
+            key = self._key_name(granularity, timestamp)
+            value= self._client.zcount(key, timestamp, timestamp)
+            results.append({'timestamp': timestamp, 'value':value})
+            timestamp += duration
+
+        return results
+
+    def _key_name(self, granularity, timestamp_in_seconds):
+        last_key_part = self._rounded_timestamp(timestamp_in_seconds, granularity['hash_size'])
+        return ':'.join([self._namespace, granularity['name'], str(last_key_part)])
+
+    def _rounded_timestamp(self, timestamp_in_seconds, precision):
+        return int((timestamp_in_seconds // precision) * precision)
